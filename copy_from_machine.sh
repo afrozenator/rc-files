7fa5ac0d415c78d9f1d745847e86a9e278faae81
Afroz Mohiuddin <afrozenator@gmail.com>
2024-06-02 07:25:49 -0700

    vim

--- copy_from_machine.sh
+++ copy_from_machine.sh
@@ -8,3 +8,6 @@ cp ~/.tmux.conf tmux/tmux.conf
 cp ~/.bashrc bash/bashrc
 
 cp ~/.pylintrc pylint/pylintrc
+
+cp ~/.vimrc vim/vimrc
+cp ~/.vim/plugin/google_python_style.vim vim/google_python_style.vim
--- copy_to_machine.sh
+++ copy_to_machine.sh
@@ -8,3 +8,6 @@ cp tmux/tmux.conf ~/.tmux.conf
 cp bash/bashrc ~/.bashrc 
 
 cp pylint/pylintrc ~/.pylintrc
+
+cp vim/vimrc ~/.vimrc
+cp vim/google_python_style.vim ~/.vim/plugin/google_python_style.vim
