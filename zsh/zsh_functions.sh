e5589d6229cebabda8007cc3e6ab1cba8ba11912
Afroz Mohiuddin <afrozenator@gmail.com>
2024-06-13 07:10:12 -0700

    bash script to add markdown toc

--- zsh/zsh_functions.sh
+++ zsh/zsh_functions.sh
@@ -42,3 +42,12 @@ function .. {
   cd $path
 }
 
+# Generate ToC for the README.md file.
+function generate_toc() {
+  # From
+  # https://medium.com/@acrodriguez/one-liner-to-generate-a-markdown-toc-f5292112fd14
+  cat /Users/afroz/Code/recipes/Recipes.md | \
+  grep -E "^#{${1:-1},${2:-5}} " | \
+  sed -E 's/(#+) (.+)/\1:\2:\2/g' | \
+  awk -F ":" '{ gsub(/#/,"  ",$1); gsub(/[ ]/,"-",$3); print $1 "- [" $2 "](#" tolower($3) ")" }'
+}
